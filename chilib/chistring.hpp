a68d8a9b0238d8c20b3316b40d02a1f33efdf028
Chiro <chiro2001@163.com>
2021-04-08 22:59:26 +0800

    vector

--- chilib/chistring.hpp
+++ chilib/chistring.hpp
@@ -1,42 +1,90 @@
+// Last modified at 2021-04-07 by Chiro
+
+#ifndef CHILIB_CHISTRING_HPP
+#define CHILIB_CHISTRING_HPP
+
 #include <cstring>
+#include <sstream>
+#include <memory>
+#include "iterator_base.hpp"
 
 namespace chilib {
 
 //typedef char char;
 
-class chistring {
+class string : public iterator_base {
 private:
   char *mdata = nullptr;
   size_t msize = 0;
-public:
+
   // 构造重用部分
-  inline void create_from_str(const char *str) {
-    if (str == nullptr) {
-      // 默认需要加上内存分配，一个\\0
+  inline void create_from_str(const char *str, size_t size = 0) {
+    if (size == 0) {
+      if (str == nullptr) {
+        // 默认需要加上内存分配，一个\\0
+        this->mdata = new char[1];
+        this->mdata[0] = '\0';
+      } else {
+        this->mdata = new char[this->msize + 1];
+        this->msize = strlen(str);
+        strcpy(this->mdata, str);
+      }
+    } else {
+      if (str == nullptr) {
+        this->mdata = new char[size];
+        this->mdata[0] = '\0';
+      } else {
+        auto len = strlen(str) > size ? strlen(str) : size;
+        this->mdata = new char[len];
+        this->msize = len;
+      }
+    }
+
+  }
+
+public:
+
+  // 默认构造函数
+  string(const char *str, size_t size = 0) { // NOLINT(google-explicit-constructor)
+    this->create_from_str(str, size);
+  }
+
+  // 分配内存
+  explicit string(size_t s) {
+    if (s == 0) {
       this->mdata = new char[1];
       this->mdata[0] = '\0';
     } else {
-      this->msize = strlen(str);
-      this->mdata = new char[this->msize + 1];
-      strcpy(this->mdata, str);
+      this->mdata = new char[s];
+      this->msize = s;
     }
   }
 
-  // 默认构造函数
-  explicit chistring(const char *str = nullptr) {
-    this->create_from_str(str);
-  }
-
   // 拷贝构造函数
-  chistring(const chistring &t) {
+  string(const string &t) {
     this->create_from_str(t.mdata);
   }
 
   // 析构函数
-  ~chistring() { delete[] this->mdata; }
+  ~string() { delete[] this->mdata; }
+
+  // 迭代器
+  class iterator : public iterator_ptr<char> {
+  public:
+    explicit iterator(char &d) : iterator_ptr<char>(d) {}
+
+    iterator &operator++() override {
+      this->node++;
+      return *this;
+    }
+  };
+
+  iterator begin() { return iterator(*this->mdata); }
+
+  iterator end() { return iterator(*(this->mdata + this->length())); }
 
   // 字符串赋值
-  chistring &operator=(const chistring &str) {
+  string &operator=(const string &str) {
 #if 0
     if (this == &str) return *this;
     delete[] this->mdata;
@@ -44,7 +92,7 @@ public:
     return *this;
 #endif
     if (this != &str) {
-      chistring temp(str);
+      string temp(str);
       // 交换内存管理
       char *t = temp.mdata;
       temp.mdata = this->mdata;
@@ -53,20 +101,40 @@ public:
     return *this;
   }
 
-  // 重载输出
-  friend std::ostream &operator<<(std::ostream &out, const chistring &str) {
-    out << str.mdata;
-    return out;
+  // 返回占用空间
+  size_t size() const {
+    return msize;
   }
 
   // 求长度
-  inline size_t length() const {
+  size_t length() const {
     return this->msize;
   }
 
   // 返回 C 风格字符串指针
-  inline const char *c_str() const {
+  const char *c_str() const {
     return (const char *) this->mdata;
+  }
+
+  // 重载输出
+  friend std::ostream &operator<<(std::ostream &out, const string &str) {
+    out << str.mdata;
+    return out;
+  }
+
+  string operator+(string &s) const {
+    // 构造新的字符串
+    string n(s.length() + this->length());
+    memcpy(n.mdata, this->mdata, this->size());
+    memcpy(n.mdata + this->msize, s.mdata, s.size());
+    return n;
+  }
+
+  string &operator=(const char *c) {
+    this->create_from_str(c);
+    return *this;
   }
 };
 }
+
+#endif // CHILIB_CHISTRING_HPP
--- chilib/linked_list.hpp
+++ chilib/linked_list.hpp
@@ -1,15 +1,31 @@
-// Last modified at 2021-04-06 by Chiro
+// Last modified at 2021-04-07 by Chiro
 
 #ifndef CHILIB_LINKED_LIST_HPP
 #define CHILIB_LINKED_LIST_HPP
 
 #include <memory>
+#include <stdexcept>
+#include <initializer_list>
+#include "iterator_base.hpp"
+#include "chistring.hpp"
 
 namespace chilib {
 
 // 单向链表，C++智能指针实现
 template<typename T>
-class linked_list {
+class linked_list : public iterator_base {
+private:
+  std::shared_ptr<linked_list<T>> next = nullptr;  // 指向下一个节点的指针
+  T data{};                                        // 当前数据
+
+  void range_check(size_t pos) {
+    if (pos >= this->size()) {
+      char buf[512];
+      sprintf(buf, "linked_list::range_check: pos (which is %d) >= this->size() (which is %d)", pos, this->size());
+      throw std::out_of_range(buf);
+    }
+  }
+
 public:
   linked_list() = default;
 
@@ -19,11 +35,39 @@ public:
 
   ~linked_list() = default;
 
-  std::shared_ptr<linked_list<T>> next = nullptr;  // 指向下一个节点的指针
-  T data{};                                        // 当前数据
+  auto get_next() { return next; }
 
-  // return: 得到链表尾部
-  // @notice  : 只有自己一个节点的时候也返回nullptr
+  T &get_data() { return data; }
+
+  /*!
+   * 得到链表大小（长度）
+   * @return 大小（长度）
+   * @note 会忽略头结点
+   */
+  size_t size() {
+    size_t s = 0;
+    auto p = this->next;
+    if (p == nullptr) return 0;
+    while (p != nullptr) {
+      p = p->next;
+      s++;
+    }
+    return s;
+  }
+
+  /*!
+   * 得到链表长度
+   * @return 长度
+   * @note 会忽略头结点
+   */
+  size_t length() {
+    return this->size();
+  }
+
+  /*!
+   * 得到链表尾部指针
+   * @return
+   */
   std::shared_ptr<linked_list<T>> get_tail() {
     auto p = this->next;
     if (p == nullptr) return nullptr;
@@ -31,26 +75,60 @@ public:
     return p;
   }
 
-  // @prog    : 连接一个节点到this节点后面
-  // @notice  : 这个传入的节点之后的数据被释放
+  /*!
+   * 得到链表尾部的前一个指针
+   * @return 节点指针
+   */
+  std::shared_ptr<linked_list<T>> get_tail_pre() {
+    auto p = this->next;
+    if (p == nullptr) return nullptr;
+    if (p->next == nullptr) return nullptr;
+    while (p->next->next != nullptr) p = p->next;
+    return p;
+  }
+
+  /*!
+   * 连接一个节点到this节点后面
+   * @param d 所要添加的节点数据
+   * @return 节点对应指针
+   * @note 这个传入的节点之后的数据被释放
+   */
   auto link(T &d) {
     auto p = linked_list::make(d);
     this->link(p);
     return p;
   }
 
-  // @prog    : 接一个已经存在的链表串到本节点之后
-  // @notice  : 本节点之后的数据释放
-  //            参数节点之后的数据保留
+  /*!
+   * 连接一个节点到this节点后面
+   * @param d 所要添加的节点数据
+   * @return 节点对应指针
+   * @note 这个传入的节点之后的数据被释放
+   */
+  auto link(T *d) {
+    auto p = linked_list::make(*d);
+    this->link(p);
+    return p;
+  }
+
+  /*!
+   * 接一个已经存在的链表串到本节点之后
+   * @param p 链表串
+   * @return 连接完成的链表串
+   * @note 本节点之后的数据释放，参数节点之后的数据保留
+   */
   std::shared_ptr<linked_list<T>> link(std::shared_ptr<linked_list<T>> &p) {
     if (p == nullptr) return nullptr;
     this->next = p;
     return p;
   }
 
-  // @prog     : 接一个已经存在的链表串到本节点之后
-  // @notice   : 本节点之后的数据接到参数节点之后
-  //             参数节点之后的数据保留到串中
+  /*!
+   * 接一个已经存在的链表串到本节点之后
+   * @param p 链表串
+   * @return 连接完成的链表串
+   * @note 本节点之后的数据接到参数节点之后，参数节点之后的数据保留到串中
+   */
   std::shared_ptr<linked_list<T>> append(std::shared_ptr<linked_list<T>> &p) {
     if (p == nullptr) return nullptr;
     auto tail = p->get_tail();
@@ -59,82 +137,156 @@ public:
     return p;
   }
 
-  // @prog    : 获取往下几个节点的指针
-  // @notice  : 不能返回自己的指针
+  /*!
+   * 获取往下几个节点的指针
+   * @param s 往下 s + 1 个节点
+   * @return 节点指针
+   * @note 不能返回自己的指针
+   */
   std::shared_ptr<linked_list<T>> step(size_t s) {
-    if (!s) return nullptr;
     auto p = this->next;
-    while (s) {
+    while (s--) {
       if (p == nullptr) return p;
       p = p->next;
-      s--;
     }
     return p;
   }
 
+/*!
+ * 从单个数据形成一个单节点链表
+ * @deprecated 应该留出头指针
+ * @param d 数据
+ * @return 链表首节点
+ */
   static auto make(T &d) {
     return std::make_shared<linked_list<T>>(d);
   }
 
+  static auto make(std::initializer_list<T> li) {
+    auto h = make_head();
+    if (li.size() == 0) return h;
+    auto p = h;
+    for (auto d : li) {
+      p = p->link(d);
+    }
+    return h;
+  }
+
+/*!
+ * 形成链表头指针
+ * @return 链表首节点
+ */
   static auto make_head() {
     return std::make_shared<linked_list<T>>();
   }
 
+/*!
+ * 寻找链表是否存在环
+ * @param p 链表
+ * @return 是否存在环
+ */
   static bool check_circulation(std::shared_ptr<linked_list<T>> &p) {
     // 快慢指针法
     auto p1 = p, p2 = p;
     do {
-      p1 = p1->step(1);
-      p2 = p2->step(2);
+      p1 = p1->step(0);
+      p2 = p2->step(1);
       if (p1 == nullptr || p2 == nullptr) return false;
     } while (p1 != p2);
     return true;
   }
 
-  // 支持迭代器
-  template<class T2, class ref, class ptr>
-  class linked_list_iterator {
+/*!
+ * 迭代器
+ */
+  class iterator : public iterator_shared_ptr<linked_list<T>> {
   public:
-    using self = linked_list_iterator<T2, ref, ptr>;
-    using self_const = const linked_list_iterator<T2, ref, ptr>;
-    std::shared_ptr<linked_list<T2>> node;
+    explicit iterator(std::shared_ptr<linked_list<T>> d) : iterator_shared_ptr<linked_list<T>>(d) {}
 
-    explicit linked_list_iterator(std::shared_ptr<linked_list<T2>> &list) : node(list) {}
-
-    ref operator*() { return node->data; }
-
-    ptr operator->() { return &(operator*()); }
-
-    // ++it
-    self &operator++() {
-      node = node->next;
+    iterator &operator++() override {
+      this->node = this->node->next;
       return *this;
-    }
-
-    // it++
-    self operator++(int) {
-      // 保存之前的迭代器
-      self tmp(*this);
-      node = node->next;
-      return tmp;
-    }
-
-    bool operator==(const self &s) {
-      return node == s.node;
-    }
-
-    bool operator!=(const self &s) {
-      return node != s.node;
     }
   };
 
-  using iterator = linked_list_iterator<T, T &, T *>;
-  using const_iterator = linked_list_iterator<T, const T &, const T *>;
-
-  // 由于没法指向自己，所以只能指向下一个。使用的时候空出头结点
+/*!
+ * 得到开始迭代器
+ * @note 由于没法指向自己，所以只能指向下一个。使用的时候空出头结点
+ * @return 迭代器
+ */
   iterator begin() { return iterator(this->next); }
 
+/*!
+ * 得到尾部迭代器
+ * @return 迭代器
+ */
   iterator end() { return iterator(this->get_tail()->next); }
+
+/*!
+ * 向尾部添加元素
+ * @param d 元素
+ * @return 添加的节点指针
+ */
+  auto push_back(T &d) {
+    auto p = this->make(d);
+    auto tail = this->get_tail();
+    if (tail == nullptr) this->link(p);
+    else this->get_tail()->link(p);
+    return p;
+  }
+
+/*!
+ * 向尾部添加元素
+ * @param d 元素
+ * @return 添加的节点指针
+ */
+  auto emplace_back(T &d) {
+    return this->push_back(d);
+  }
+
+/*!
+ * 向尾部添加元素
+ * @param d 元素
+ * @return 添加的节点指针
+ */
+  auto emplace_back(T d) {
+    return this->push_back(d);
+  }
+
+/*!
+ * 向尾部添加元素
+ * @param d 元素
+ * @return 添加的节点指针
+ */
+  auto emplace_back(T *d) {
+    return this->push_back(*d);
+  }
+
+/*!
+ * 删除尾部元素
+ */
+  void pop_back() noexcept {
+    auto pre = this->get_tail_pre();
+    if (pre == nullptr) {
+      if (this->next != nullptr) {
+        this->next = nullptr;
+      }
+      return;
+    }
+    pre->next = nullptr;
+  }
+
+  T &at(size_t pos) {
+    this->range_check(pos);
+    auto p = this->step(pos);
+    return p->data;
+  }
+
+  iterator insert(const iterator &it, T &d) {
+    auto n = (*it)->get_next();
+    auto p = (*it)->link(d);
+    p->get_next() = n;
+  }
 };
 
 // 外部可以使用 linked_list_p 做指针简单名字
